f77bda41b9e57e1d463ea0d022f653f0883e1f18
Ankit Kumar <ankitkr00.india@gmail.com>
2021-09-09 01:48:39 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Header'
+function App() {
+  return (
+    // BEM naming convention 
+    <div className="app">
+      {/* <h1>Hello, My name is Ankit Kumar and I'm gonna build the Amazon Store </h1> */}
+      <Header />
+      {/*Body */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDWVpIh0-MEJen5FFsatjZqr_x2QrhMtTM",
+    authDomain: "clone-7f2da.firebaseapp.com",
+    projectId: "clone-7f2da",
+    storageBucket: "clone-7f2da.appspot.com",
+    messagingSenderId: "370271831814",
+    appId: "1:370271831814:web:ebd3e11c5c562f81fe4087",
+    measurementId: "G-D48R3LD1T2"
+  };
