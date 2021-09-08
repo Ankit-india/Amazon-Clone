229756c4c2cf95a2ce37050acc1b29f6a0033dd6
Ankit Kumar <ankitkr00.india@gmail.com>
2021-09-09 02:24:37 +0530

    Added home page files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import Header from './Header'
+import Header from './Header';
+import Home from './Home';
 function App() {
   return (
     // BEM naming convention 
     <div className="app">
       {/* <h1>Hello, My name is Ankit Kumar and I'm gonna build the Amazon Store </h1> */}
       <Header />
-      {/*Body */}
+      <Home />
     </div>
   );
 }
